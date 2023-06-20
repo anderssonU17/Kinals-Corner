d29704d9afc16dc502197ac6316ad74f34c91465
jcastro2021416 <jcastro-2021416@kinal.edu.gt>
2023-06-20 00:01:00 -0600

    Se agrego base del proyecto

---
+++ Backend/src/models/teacher.model.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema({
+    name: { 
+        type: String,
+        required: true 
+    },
+    photo: { 
+        type: String 
+    },
+    subject: { 
+        type: String,
+        required: true
+    }
+  });
+  
+module.exports = mongoose.model('Teacher', teacherSchema);
