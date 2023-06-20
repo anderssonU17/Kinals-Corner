73ffcb7501929458bc1e2934b4c8bb4771c5b742
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-06-19 23:32:50 -0600

    Se agregon modelos y validate-params en backend

---
+++ Backend/src/models/helpSocial.model.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const helpSocialSchema = new Schema({
+    title: { 
+        type: String, 
+        required: true 
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    image: { 
+        type: String 
+    },
+    claimed: { 
+        type: Boolean, 
+        default: false 
+    },
+    claimDate: { 
+        type: Date 
+    },
+    claimantName: { 
+        type: String 
+    },
+});
+
+module.exports = mongoose.model('HelpSocial', helpSocialSchema);
