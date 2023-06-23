50a89d051b5abc39f4d3f37285361f3dd67eb2fa
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-06-23 16:05:49 -0600

    Se agrego Frontend de epatzan a aurrea

---
+++ Frontend/src/AppRouter.jsx
@@ -0,0 +1,35 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Login } from "./Login/components/Login";
+import { isUserAuthenticated } from "./Login/helpers/LoginHelper";
+
+
+export const AppRouter = () => {
+    return(
+        <>
+        
+        <Routes>
+
+            <Route  path="/" 
+            element={
+                isUserAuthenticated() ? (
+                <UserTable />
+                ) : (
+                    <Navigate to="/login"></Navigate>
+                    )
+                }
+            ></Route>
+
+            <Route path="/login" 
+            element={
+                !isUserAuthenticated() ? (
+                    <Login></Login>
+                    ) : (
+                    <Navigate to="/"></Navigate>
+                )
+            }
+            ></Route>
+
+        </Routes>
+        </>
+    );
+};
