0d9d1861bb3c4522bccaf1aec4b50ec2ff81d95a
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-06-23 10:18:58 -0600

    Se corrigio archivo de jcastro

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,6 @@ require('dotenv').config;
 const port = process.env.PORT;
 const cors = require("cors");
 
-const routesUser = require('./src/routes/user.routes')
 const routesHelpSocial = require('./src/routes/helpSocial.routes')
 connection();
 
@@ -16,7 +15,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', routesUser, routesHelpSocial);
+app.use('/api', routesHelpSocial);
 
 
 app.listen(port, ()=> {
