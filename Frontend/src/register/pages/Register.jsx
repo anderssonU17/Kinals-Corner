f1a8b164bff0cc411bda054da1ee0943fcf3f4b9
Jflores-2019424 <107268421+Jflores-2019424@users.noreply.github.com>
2023-06-22 22:36:34 -0600

    Register - vista con css

---
+++ Frontend/src/register/pages/Register.jsx
@@ -0,0 +1,33 @@
+import './register.css'
+import logo from "./assets/image/LogoHd.png"
+
+export const  Register= () => {
+
+  return (
+    <>
+      <div className='container container-center'>
+        <div className='container-center'>
+            <div className="container-centers">
+              <img src={logo} className='image'/>                
+                <h1 className='container-center'>
+                  Registrarse
+                </h1>
+            </div>
+        </div>
+            <div className="container-center row">
+              <input className='input' type="text" placeholder='Nombres' /><br />
+              
+              <input className='input' type="text" placeholder='Apellidos' /><br />
+              
+              <input className='input' type="text" placeholder='Correo'/><br />
+              
+              <input className='input' type="password" placeholder='Contraseña'/><br />
+
+              <div className='container-right row'>
+                 <button className='btn'>Registrarse</button>
+              </div>
+            </div>
+      </div>
+    </>
+  )
+}
