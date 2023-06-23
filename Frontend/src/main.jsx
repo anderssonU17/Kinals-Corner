2d9b90a68dcce87778c20db8eac5cd08341be9d0
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-06-23 15:32:45 -0600

    Se combino con la rama de jflores

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,8 +4,6 @@ import { AppRouter } from './AppRouter'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import './styles.css'
-import { AppRouter } from './AppRouter.jsx'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
