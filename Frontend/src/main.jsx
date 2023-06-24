5aaca7d6c0f5878ff2f21cc455ebd95b088259b1
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-06-24 11:56:15 -0600

    Se reviso rama de jflores y se revisaron vistas

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { AppRouter } from './AppRouter.jsx'
+import { Register } from './register/pages/Register'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <AppRouter />
+    <Register />
     </BrowserRouter>
   </React.StrictMode>,
 )
