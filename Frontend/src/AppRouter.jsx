f6fd0c5dcb98a58f01e2a8c033a01957b2e97837
HumbertoLopez2020327 <jherrera-2020327@kinal.edu.gt>
2023-07-03 19:41:11 -0600

    Se agregó la carpeta frontend a la rama jherrera

---
+++ Frontend/src/AppRouter.jsx
@@ -0,0 +1,36 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { isUserAuthenticated } from "./auth/helpers/LoginHelper";
+import { Register } from "./register/pages/Register";
+import { Login } from "./auth/pages/Login";
+import { Foro } from "./foro/pages/Foro";
+
+export const AppRouter = () => {
+    return(
+        <>
+        
+        <Routes>
+
+            <Route  path="/" 
+            element={
+                isUserAuthenticated() ? (
+                <UserTable />
+                ) : (
+                    <Navigate to="/login"></Navigate>
+                    )
+                }
+            ></Route>
+
+            <Route path="/login" 
+            element={
+                !isUserAuthenticated() ? (
+                    <Register></Register>
+                    ) : (
+                    <Navigate to="/"></Navigate>
+                )
+            }
+            ></Route>
+
+        </Routes>
+        </>
+    );
+};
