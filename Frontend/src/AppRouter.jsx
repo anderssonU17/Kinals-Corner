2647bf7f8120a2ce22b60e9f93e3d16fc8cb2c7c
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-07-21 04:51:55 -0600

    Se revisaron errores

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -1,15 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { isUserAuthenticated } from "./auth/helpers/LoginHelper";
-import { Navbar } from "./components/Navbar";
 import { Login } from "./auth/pages/Login";
 import { Teachers } from "./teachers/pages/Teachers";
+import { HomePage } from "./home/pages/HomePage";
 
 export const AppRouter = () => {
     return(
         <>
         <Routes>
         <Route path="/" 
-            element={isUserAuthenticated() ? <Navbar/> : <Navbar/>}
+            element={isUserAuthenticated() ? <HomePage/> : <HomePage/>}
         />
 
 
