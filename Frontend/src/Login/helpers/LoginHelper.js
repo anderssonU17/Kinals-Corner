00365123ed5eb6f688ba06bad0bfad681334c670
epatzan <epatzan-2021484.@kinal.edu.gt>
2023-07-20 21:17:23 -0600

    Se eliminarion archivos inecesarios

--- Frontend/src/Login/helpers/LoginHelper.js
+++
@@ -1,8 +0,0 @@
-
-
-export const isUserAuthenticated = () =>{
-    if(localStorage.getItem("token")){
-        return true;
-    }
-    return false;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Kinals-Corner
-Welcome to Kinal's Corner! Explore and discover our institution's insights and updates.
