609c32477995615d02158754716959fffd9b3843
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-07-21 04:21:33 -0600

    Se agrego pagina de inicio con CSS

--- Frontend/src/foro/pages/Foro.jsx
+++ Frontend/src/foro/pages/Foro.jsx
@@ -1,83 +1,86 @@
-import {useState} from 'react'
-import { Publicacion } from "./Publicacion";
-import "../../assets/styles/Foro.css"
+import React, { useState, useEffect } from 'react';
+import { Publicacion } from './Publicacion';
+import '../../assets/styles/Foro.css';
+import { createForum } from '../api/ApiForo';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+import { Link } from 'react-router-dom';
+
 
 export const Foro = () => {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  
+  const [content, setContent] = useState('');
 
-  
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    if (name === 'title') {
-      setTitle(value);
-    } else if (name === 'description') {
-      setDescription(value);
+  const publicar = async (e) => {
+    e.preventDefault();
+    if (title.trim() === "" || content.trim() === "") { {/* verifica si algun input esta vacio, para no permitir hacer la publicacion */}
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Por favor, completa ambos campos antes de publicar',
+      });
+      return;
     }
-  };
-
-  const addTask = () => {
-    if (title && description) {
-      const newTask = { title, description };
-      setTasks((prevTasks) => [...prevTasks, newTask]);
+    try {
+      const result = await createForum(title, content);
+      setTasks((prevTasks) => [...prevTasks, result]); // Agregar la nueva publicación al estado
       setTitle('');
-      setDescription('');
+      setContent('');
+    } catch (error) {
+      console.error('Error al publicar:', error);
     }
   };
-  
+
   return (
-    <div> 
-    
-      <div className="MainDiv">
-      <h2 >
-        Foro</h2>
-    <hr />
-    
-    <div className="postDiv">
-            <Publicacion/>
-    </div>
-     <div className="mensageSaliente">
-      <ul>
-            {tasks.map((task, index) => (
-              <li key={index}>
-                <label for="floatingTextarea2" className="mensage1">Nombre Usuario</label>
-                {/* Titulo y descripcion del mensage */}
-                <h3 className="titulo">{task.title}</h3>
-                <p className="descripcion">{task.description}</p>
-              </li>
-            ))}
-          </ul>
-     </div>
-    
-    <hr />
-    <div className="form">
-      <div className="input-group col-auto">
-        <input
-          type="text"
-          name="title"
-          placeholder="Título"
-          className="form-control input"
-          value={title}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="description"
-          placeholder="Descripción"
-          className="form-control description input"
-          value={description}
-          onChange={handleInputChange}
-        />
-        <button className='button' onClick={addTask}>Publicar</button>
+    <>
+    <div className='mapa-div'>
+          Algo mas
+        </div>
+    <div className='main-container'>
+        <div className="MainDiv">
+          <h2>Foro</h2>
+          
+          <div className="form">
+            <div className="input-group col-auto">
+              <input
+                type="text"
+                name="title"
+                placeholder="Título"
+                className="form-control input"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <input
+                type="text"
+                name="description"
+                placeholder="Descripción"
+                className="form-control description input"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+              <button className='button' onClick={publicar}>Publicar</button>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className="mensageSaliente postDiv">
+              mensaje
+          </div>
+          <hr />
+
+
+          <div className="mb-3">
+            <Publicacion tasks={tasks} /> {/* Pasar el estado tasks a Publicacion */}
+          </div>
+          <hr />
+        </div>
+        
+        <div className='mapa-div'>
+          mapa
+        </div>
       </div>
-    </div>
-    </div>
-    {/* <div>
-            <Publicacion></Publicacion>
-    </div> */}
-    </div>
-  )
-}
+      </>
+  );
+};
--- Frontend/src/foro/pages/Publicacion.jsx
+++ Frontend/src/foro/pages/Publicacion.jsx
@@ -1,38 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // para descargar la libreria yarn add @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import '../../assets/styles/Publicacion.css'
+import { readForum } from "../api/ApiForo";
 
-export const Publicacion = () => {
-const [publicacion, setPublicacion] = useState([]);
-const [likesCount, setLikesCount] = useState(0);
-const [isLiked, setIsLiked] = useState(false);
+export const Publicacion = ({ tasks }) => {
+    const [publicacion, setPublicacion] = useState([]);
+    useEffect(() => {
+    const fetchData = async () => {
+        const result = await readForum();
+        setPublicacion(result);
+    };
+    fetchData();
+    }, [tasks]);
 
-const handleLikeClick = () => {
-    if (isLiked) {
-    setLikesCount(likesCount - 1);
-    } else {
-    setLikesCount(likesCount + 1);
-    }
-    setIsLiked(!isLiked);
-};
-
+    const handleLikeClick = (index) => {
+        setPublicacion((prevPublicacion) => {
+        const updatedPublicacion = [...prevPublicacion];
+        const post = updatedPublicacion[index];
+        if (post.isLiked) {
+            post.likesCount -= 1;
+        } else {
+            post.likesCount += 1;
+        }
+        post.isLiked = !post.isLiked;
+        return updatedPublicacion;
+    });
+    };
     return (
-    <>
-    <div>
-        <div>
-        <img src="" alt="" />
-        <label htmlFor="">NOMBRE USER</label>
-        <p>Info</p>
-        <button
-                className={`like-button ${isLiked ? "liked" : ""}`}
-            onClick={handleLikeClick}
-        >
-            <FontAwesomeIcon icon={faFire} />
-        </button>
-        <span className="likes-count">{likesCount}</span>
-        </div>
-    </div>
-    </>
-);
+        <>
+            <div className="post-container">
+            {publicacion?.map((publicacionActual, index) => (
+                <div key={publicacionActual._id}>
+                <h5>{publicacionActual.title}</h5>
+                <p>{publicacionActual.content}</p>
+                <button
+                    className={`like-button ${
+                    publicacionActual.isLiked ? "liked" : ""
+                    }`}
+                    onClick={() => handleLikeClick(index)}
+                >
+                    <FontAwesomeIcon icon={faFire} />
+                </button>
+                <span className="likes-count">{publicacionActual.likesCount}</span>
+                </div>
+            ))}
+            </div>
+        </>
+    );
 };
