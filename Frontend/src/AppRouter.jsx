b95d6448e2e3a0aa3594be6d8591660bd1d6409d
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-07-21 05:48:14 -0600

    Se eliminaron cosas innecesarias

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -1,26 +1,50 @@
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
-import { isUserAuthenticated } from "./auth/helpers/LoginHelper";
-import { Register } from "./register/pages/Register";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import { Login } from "./auth/pages/Login";
-import { Foro } from "./foro/pages/Foro";
-import { useEffect } from "react";
+import { Teachers } from "./teachers/pages/Teachers";
+import { HomePage } from "./home/pages/HomePage";
+import { Register } from "./register/pages/Register";
+import { Navbar } from "./components/Navbar";
 
 export const AppRouter = () => {
-    const navigate = useNavigate() // para redirigir a un vista en especifico
-    useEffect(() => {
-    if (!isUserAuthenticated()) { // Redirigir al usuario a la página de inicio de sesión en caso de no haber iniciado sesion 
-    navigate("/login");
-    }
-    }, [navigate]);
+    return(
+        <>
+        {isUserAuthenticated() && <Navbar/>}
+        <Routes>
+        <Route path="/" 
+            element={isUserAuthenticated() ? <Teachers/> : <HomePage/>}
+        />
 
-    return (
-    <>
-        <Routes> 
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/foro" element={<Foro />} />
+
+            <Route path="/login" 
+            element={
+                !isUserAuthenticated() ? (
+                    <Login/>
+                    ) : (
+                    <Navigate to="/"></Navigate>
+                )
+            }
+            />
+            <Route path="/register" 
+            element={
+                !isUserAuthenticated() ? (
+                    <Register/>
+                    ) : (
+                    <Navigate to="/"></Navigate>
+                )
+            }
+            />
+            <Route path="/teachers" 
+            element={
+                isUserAuthenticated() ? (
+                    <Teachers></Teachers>
+                    ) : (
+                    <Navigate to="/"></Navigate>
+                )
+            }
+            />
+
         </Routes>
-    </>
+        </>
     );
 };
